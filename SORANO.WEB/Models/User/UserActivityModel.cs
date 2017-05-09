5720b1dbaaf57cd7687e1b3513d4ba6719ecaf72
Pavlo Grushetsky <pavlo.grushetsky@gmail.com>
2017-05-09 14:30:19 +0300

    Added display names to model

--- SORANO.WEB/Models/User/UserActivityModel.cs
+++ SORANO.WEB/Models/User/UserActivityModel.cs
@@ -1,13 +1,31 @@
-﻿namespace SORANO.WEB.Models.User
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SORANO.WEB.Models.User
 {
     public class UserActivityModel
     {
+        /// <summary>
+        /// Activity type
+        /// </summary>
+        [Display(Name = "Тип действия")]
         public string ActivityType { get; set; }
 
+        /// <summary>
+        /// Entity identifier
+        /// </summary>
+        [Display(Name = "ID сущности")]
         public int EntityID { get; set; }
 
+        /// <summary>
+        /// Entity type
+        /// </summary>
+        [Display(Name = "Тип сущности")]
         public string EntityType { get; set; }
 
+        /// <summary>
+        /// Activity date
+        /// </summary>
+        [Display(Name = "Дата")]
         public string Date { get; set; }
     }
 }
