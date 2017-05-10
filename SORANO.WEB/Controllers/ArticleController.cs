28a9c53ee8e36aac29df73f080a751bd04d4f3e5
Pavlo Grushetsky <pavlo.grushetsky@gmail.com>
2017-05-10 23:44:12 +0300

    Started to implement Articles functionality

---
+++ SORANO.WEB/Controllers/ArticleController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SORANO.WEB.Controllers
+{
+    [Authorize(Roles = "developer,administrator,manager")]
+    public class ArticleController : Controller
+    {
+        public IActionResult Tree()
+        {
+            return View();
+        }
+    }
+}
