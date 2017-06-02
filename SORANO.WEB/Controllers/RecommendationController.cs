76396d717865ebf9ab77e78be83396c28c407fa7
Pavlo Grushetsky <pavlo.grushetsky@gmail.com>
2017-06-02 16:30:39 +0300

    Continued to implement recommendations functionality

---
+++ SORANO.WEB/Controllers/RecommendationController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SORANO.WEB.Models.Recommendation;
+using System.Collections.Generic;
+
+namespace SORANO.WEB.Controllers
+{
+    public class RecommendationController : Controller
+    {
+        public IActionResult TableForm(List<RecommendationModel> recommendations)
+        {
+            return PartialView("_TableForm", recommendations);
+        }
+    }
+}
