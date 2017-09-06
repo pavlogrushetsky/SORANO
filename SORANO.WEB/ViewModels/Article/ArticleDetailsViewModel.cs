4a18ecd63f02096777da89924474ecfda1de7dbb
Pavlo Grushetsky <pavlo.grushetsky@gmail.com>
2017-09-06 16:26:28 +0300

    Refactored Article details functionality

--- SORANO.WEB/ViewModels/Article/ArticleDetailsViewModel.cs
+++ SORANO.WEB/ViewModels/Article/ArticleDetailsViewModel.cs
@@ -1,7 +1,31 @@
-﻿namespace SORANO.WEB.ViewModels.Article
+﻿using System.ComponentModel.DataAnnotations;
+using SORANO.WEB.ViewModels.Common;
+
+namespace SORANO.WEB.ViewModels.Article
 {
-    public class ArticleDetailsViewModel
+    public class ArticleDetailsViewModel : BaseDetailsViewModel
     {
-        
+        [Display(Name = "Название")]
+        public string Name { get; set; }
+
+        [Display(Name = "Описание")]
+        public string Description { get; set; }
+
+        [Display(Name = "Производитель")]
+        public string Producer { get; set; }
+
+        [Display(Name = "Код")]
+        public string Code { get; set; }
+
+        [Display(Name = "Штрих-код")]
+        public string Barcode { get; set; }
+
+        [Display(Name = "Тип артикулов")]
+        public string TypeID { get; set; }
+
+        [Display(Name = "Тип артикулов")]
+        public ArticleTypeModel Type { get; set; }
+
+        public string ReturnPath { get; set; }
     }
 }
---
+++ SORANO.WEB/ViewModels/Common/BaseDetailsViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using SORANO.WEB.ViewModels.Attachment;
+using SORANO.WEB.ViewModels.Recommendation;
+
+namespace SORANO.WEB.ViewModels.Common
+{
+    public class BaseDetailsViewModel
+    {
+        [Display(Name = "ID")]
+        public int ID { get; set; }
+
+        [Display(Name = "Рекомендации")]
+        public IList<RecommendationViewModel> Recommendations { get; set; }
+
+        [Display(Name = "Вложения")]
+        public IList<AttachmentViewModel> Attachments { get; set; }
+
+        public string MainPicturePath { get; set; }
+
+        public bool HasMainPicture => !string.IsNullOrEmpty(MainPicturePath);
+
+        [Display(Name = "Создание")]
+        public string Created { get; set; }
+
+        [Display(Name = "Создание")]
+        public string CreatedBy { get; set; }
+
+        [Display(Name = "Изменение")]
+        public string Modified { get; set; }
+
+        [Display(Name = "Изменение")]
+        public string ModifiedBy { get; set; }
+
+        [Display(Name = "Удаление")]
+        public string Deleted { get; set; }
+
+        [Display(Name = "Удаление")]
+        public string DeletedBy { get; set; }
+
+        [Display(Name = "Статус")]
+        public bool IsDeleted { get; set; }
+    }
+}
