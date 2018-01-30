fec3c64a622b6ae3de497fd2dee0817e8daaff51
Pavlo Grushetsky <pavlo.grushetsky@gmail.com>
2018-01-30 21:04:24 +0200

    Started to implement Sale Create page

--- SORANO.WEB/ViewModels/Sale/SaleCreateUpdateViewModel.cs
+++ SORANO.WEB/ViewModels/Sale/SaleCreateUpdateViewModel.cs
@@ -1,9 +1,31 @@
 ﻿using SORANO.WEB.ViewModels.Common;
+using System.ComponentModel.DataAnnotations;
 
 namespace SORANO.WEB.ViewModels.Sale
 {
     public class SaleCreateUpdateViewModel : BaseCreateUpdateViewModel
     {
-        
+        [Display(Name = "Место продажи *")]
+        public int LocationID { get; set; }
+
+        public string LocationName { get; set; }
+
+        [Display(Name = "Клиент")]
+        public int ClientID { get; set; }
+
+        public string ClientName { get; set; }
+
+        public string TotalPrice { get; set; }
+
+        public string SelectedCurrency { get; set; } = "₴";
+
+        [Display(Name = "Дата продажи")]
+        public string Date { get; set; }
+
+        public string DollarRate { get; set; }
+
+        public string EuroRate { get; set; }
+
+        public bool IsSubmitted { get; set; }
     }
 }
