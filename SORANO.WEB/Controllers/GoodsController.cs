aece41c4278e0e8f622000bc9069a82961e7a6b5
Pavlo Grushetsky <pavlo.grushetsky@gmail.com>
2018-03-03 11:51:44 +0200

    Removed unused stuff

--- SORANO.WEB/Controllers/GoodsController.cs
+++ SORANO.WEB/Controllers/GoodsController.cs
@@ -4,10 +4,8 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using SORANO.BLL.Services.Abstract;
 using SORANO.WEB.Infrastructure.Filters;
-using SORANO.WEB.ViewModels;
 using SORANO.WEB.ViewModels.Goods;
 using System.Collections.Generic;
-using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 using SORANO.BLL.Dtos;
@@ -21,8 +19,6 @@ namespace SORANO.WEB.Controllers
     public class GoodsController : BaseController
     {
         private readonly IGoodsService _goodsService;
-        private readonly ILocationService _locationService;
-        private readonly IArticleService _articleService;
         private readonly IMapper _mapper;
 
         public GoodsController(IGoodsService goodsService, 
@@ -32,8 +28,6 @@ namespace SORANO.WEB.Controllers
             IMapper mapper) : base(userService)
         {
             _goodsService = goodsService;
-            _locationService = locationService;
-            _articleService = articleService;
             _mapper = mapper;
         }
 
@@ -103,98 +97,6 @@ namespace SORANO.WEB.Controllers
             {
                 TempData["Error"] = ex;
                 return RedirectToAction("Index", "Goods");
-            });
-        }
-
-        //[HttpGet]
-        //public async Task<IActionResult> Expand(int articleId, int articleTypeId, int locationId, int status)
-        //{
-        //    return await TryGetActionResultAsync(async () =>
-        //    {
-        //        ModelState.RemoveFor("IsFiltered");
-
-        //        var goodsResult = await _goodsService.GetAllAsync(articleId, articleTypeId, locationId, true, status);
-
-        //        if (goodsResult.Status != ServiceResponseStatus.Success)
-        //        {
-        //            TempData["Error"] = "Не удалось получить список товаров.";
-        //            return RedirectToAction("Index", "Home");
-        //        }
-
-        //        var goods = _mapper.Map<IEnumerable<GoodsItemViewModel>>(goodsResult.Result).ToList();
-        //        var first = goods.First();
-        //        var viewModel = new GoodsIndexViewModel
-        //        {
-        //            Goods = goods.ToList(),
-        //            ArticleID = articleId,
-        //            ArticleName = first.ArticleName,
-        //            ArticleTypeID = articleTypeId,
-        //            ArticleTypeName = first.ArticleTypeName,
-        //            LocationID = locationId,
-        //            LocationName = first.LocationName,
-        //            ShowByPiece = true,
-        //            IsFiltered = true,
-        //            Status = status
-        //        };
-
-        //        return View("Index", viewModel);
-        //    }, ex =>
-        //    {
-        //        TempData["Error"] = ex;
-        //        return RedirectToAction("Index", "Goods");
-        //    });
-        //}
-
-        [HttpGet]
-        public async Task<IActionResult> Sales()
-        {
-            var goods = await _goodsService.GetSoldGoodsAsync();
-
-            var sales = goods.Result.GroupBy(g => new { g.ClientID, g.DeliveryItem.ArticleID, g.SaleLocationID, g.SaleDate.Value.Date, g.SalePrice })
-                    .Select(g => new SaleModel
-                    {
-                        ClientID = g.Key.ClientID.Value,
-                        ClientName = g.First().Client.Name,
-                        ArticleID = g.Key.ArticleID,
-                        ArticleName = g.First().DeliveryItem.Article.Name,
-                        Count = g.Count(),
-                        LocationID = g.Key.SaleLocationID ?? 0,//TODO
-                        LocationName = g.First().SaleLocation.Name,
-                        TotalPrice = (g.Count() * g.Key.SalePrice.Value).ToString("0.00") + " ₴",
-                        SaleDate = g.Key.Date.ToString("dd.MM.yyyy")
-                    }).ToList();
-
-            return View(sales);
-        }
-
-        [HttpGet]
-        public async Task<IActionResult> Sale(int? articleId, int? currentLocationId, int? maxCount, string returnUrl)
-        {
-            var locationName = string.Empty;
-            if (currentLocationId.HasValue)
-            {
-                var location = await _locationService.GetAsync(currentLocationId.Value);
-                locationName = location.Result?.Name;
-            }
-
-            var articleName = string.Empty;
-            if (articleId.HasValue)
-            {
-                var article = await _articleService.GetAsync(articleId.Value);
-                articleName = article.Result?.Name;//TODO
-            }
-
-            return View(new SaleModel
-            {
-                ReturnUrl = returnUrl,
-                ArticleID = articleId ?? default(int),
-                ArticleName = articleName,
-                IsArticleEditable = !articleId.HasValue,
-                LocationID = currentLocationId ?? default(int),
-                LocationName = locationName,
-                IsLocationEditable = !currentLocationId.HasValue,
-                MaxCount = maxCount ?? 1,
-                Count = 1
             });
         }
 
@@ -413,39 +315,6 @@ namespace SORANO.WEB.Controllers
             });
         }
 
-        [HttpPost]
-        public async Task<IActionResult> Sale(SaleModel model)
-        {
-            return await TryGetActionResultAsync(async () =>
-            {
-                if (!ModelState.IsValid)
-                {
-                    return View(model);
-                }
-
-                var parsed = decimal.TryParse(model.SalePrice, NumberStyles.Any, new CultureInfo("en-US"), out decimal salePrice);
-
-                await _goodsService.SaleAsync(model.ArticleID, model.LocationID, model.ClientID, model.Count, salePrice, UserId);
-
-                return Redirect(model.ReturnUrl);
-            }, ex =>
-            {
-                ModelState.AddModelError("", ex);
-
-                return View(model);
-            });
-        }
-
         #endregion 
-
-        //[HttpPost]
-        //public async Task<JsonResult> GetGoods(string term, int locationId)
-        //{
-        //    var articles = await _articleService.GetAllAsync(false, term);
-
-        //    var selectModels = _mapper.Map<IEnumerable<ArticleSelectViewModel>>(articles.Result);
-
-        //    return Json(new { results = selectModels.OrderBy(s => s.Name) });
-        //}
     }
 }
