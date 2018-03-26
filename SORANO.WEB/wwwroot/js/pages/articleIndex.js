54641d87d50e7261c39c9e65a717ece38993f40b
Pavlo Grushetsky <pavlo.grushetsky@gmail.com>
2018-03-26 20:29:21 +0300

    Fixed styles of article types tree

--- SORANO.WEB/wwwroot/js/pages/articleIndex.js
+++ SORANO.WEB/wwwroot/js/pages/articleIndex.js
@@ -6,12 +6,7 @@
     var lastTab = localStorage.getItem('articles-active-tab');
     if (lastTab) {
         $('[href="' + lastTab + '"]').tab('show');
-    }
-       
-    var activeInfoId = localStorage.getItem('article-types-active-info');
-    if (activeInfoId) {
-        $(activeInfoId).show('fast');
-    }   
+    }     
 
     loadArticleTable();
     loadArticleTypesTree();
@@ -150,7 +145,7 @@ function initArticleTypesTree() {
     $('.article-types-tree li:has(ul)').addClass('parent-article-type').find(' > table').attr('title', 'Свернуть ветку');
     $('.article-types-tree li.parent-article-type > table').on('click', function (e) {
         var target = $(e.target);
-        if (!target.is("a")) {
+        if (!target.is("a") && !target.is('i')) {
             var children = $(this).parent('li.parent-article-type').find(' > ul > li');
             if (children.is(":visible")) {
                 children.hide('fast');
@@ -162,23 +157,6 @@ function initArticleTypesTree() {
                 $(this).find(' > table').hide('fast');
             }
             e.stopPropagation();
-        }
-    });
-
-    $('.toggle-article-type-info').click(function (e) {
-        e.preventDefault();
-
-        var self = $(this);
-        var thisInfo = self.parent().parent().parent().find('.article-type-info');
-        var tree = $('.article-types-tree');
-        var id = thisInfo.attr('id');
-
-        if (thisInfo.is(':visible')) {
-            thisInfo.hide('fast');
-        } else {
-            tree.find('.article-type-info').hide('fast');
-            thisInfo.show('fast');
-            localStorage.setItem('article-types-active-info', '#' + id);
         }
     });
 }
