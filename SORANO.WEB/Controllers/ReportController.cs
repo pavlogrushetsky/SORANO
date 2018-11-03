80c71e64f9f1de329e162a2ae7b47bf59e7bb5e8
Pavlo Grushetsky <pavlo.grushetsky@gmail.com>
2018-11-03 14:59:33 +0200

    Started to implement reports functionality

---
+++ SORANO.WEB/Controllers/ReportController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SORANO.BLL.Services.Abstract;
+using SORANO.WEB.Infrastructure.Filters;
+
+namespace SORANO.WEB.Controllers
+{
+    [Authorize(Roles = "administrator,manager")]
+    [CheckUser]
+    public class ReportController : BaseController
+    {
+        public ReportController(IUserService userService, IExceptionService exceptionService) : base(userService, exceptionService)
+        {
+        }
+
+        public IActionResult Index() => View();
+    }
+}
